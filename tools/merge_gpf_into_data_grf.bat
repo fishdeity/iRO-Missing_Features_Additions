e24a877568d84f744bda6a53410cd19559079c2e
wolfhev <wolfchevlab@gmail.com>
2023-01-15 12:58:27 +0500

    - Update merge_gpf_into_data_grf to handle backup_of_originals - Change extension of .grf files in backup_of_originals to .gpf

--- tools/merge_gpf_into_data_grf.bat
+++ tools/merge_gpf_into_data_grf.bat
@@ -1,2 +1,23 @@
-for %%f in (.\*.gpf) do rsumerge data.grf %%f
-PAUSE
+@echo off
+set /p "merge=Enter 1 to merge edits or 2 to merge backups: "
+
+if %merge% == 1 goto :edits
+if %merge% == 2 goto :backups
+
+:edits
+for %%f in (.\*.gpf) do (
+   (echo "%%f" | findstr /i "original normal" 1>NUL) || (
+      rsumerge data.grf %%f
+   )
+)
+goto :pause
+
+:backups
+for %%f in (.\*.gpf) do (
+   (echo "%%f" | findstr /i /v "original normal" 1>NUL) || (
+      rsumerge data.grf %%f
+   )
+)
+
+:pause
+pause
