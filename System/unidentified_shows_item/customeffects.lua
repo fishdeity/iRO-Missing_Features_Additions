603cd9190bbab824c060db972fb47071184437a0
fishdeity <56460323+fishdeity@users.noreply.github.com>
2023-08-11 14:56:51 -0700

    .

---
+++ System/unidentified_shows_item/customeffects.lua
@@ -0,0 +1,83 @@
+customeffects = {
+    --[[
+    ! YOU DO NOT HAVE TO RE-RUN iteminfo.bat AFTER EDITING THIS FILE !
+    ! Effects added here will take priority over original iteminfo !
+
+    Refer to effect_list.txt for an (incomplete) list of effect IDs
+    1186: rare highlight (pink)
+    1187: rare highlight (white)
+    1188: rare highlight (blue)
+    1189: rare highlight (gold)
+    1190: rare highlight (purple)
+    1191: rare highlight (orange)
+    --]]
+	
+	--FishDeity Custom Additions
+	---Cards
+	[4557]	   = { EffectID = 1191 }, --Weakened Fenrir Card
+	---Equips
+	[1306]	   = { EffectID = 1187 }, --War Axe
+	---Pet Related
+	[660]	   = { EffectID = 1188 }, --Forbidden Red Candle
+	[12370]	   = { EffectID = 1188 }, --Girl's Naivety
+	[12373]	   = { EffectID = 1188 }, --Boy's Pure Heart
+	[23187]	   = { EffectID = 1188 }, --Sap Jelly
+	[23189]	   = { EffectID = 1188 }, --Small Doll Needle
+	[23256]	   = { EffectID = 1188 }, --Spiritual Bandage
+	---Consumables
+	[12040]	   = { EffectID = 1191 }, --Stone of Sage
+	[23986]	   = { EffectID = 1191 }, --Odin's Relic
+	[23985]	   = { EffectID = 1191 }, --Dragon's Treasure
+	---Drops
+	[969]	   = { EffectID = 1191 }, --Gold
+	[6960]	   = { EffectID = 1191 }, --Sky Fortress Key
+	[6608]	   = { EffectID = 1191 }, --Coagulated Spell
+	[7444]	   = { EffectID = 1191 }, --Treasure Box
+
+	--original
+	---Drops
+	[6224]	   = { EffectID = 1191 }, --Bradium
+	[9517]	   = { EffectID = 1191 }, --Unidentified Ore
+	[25271]	   = { EffectID = 1191 }, --Illusion Stone
+	[25728]	   = { EffectID = 1191 }, --Shadowdecon Ore
+	[25729]	   = { EffectID = 1191 }, --Shadowdecon
+	[25730]	   = { EffectID = 1191 }, --Zelunium Ore
+	[25731]	   = { EffectID = 1191 }, --Zelunium
+	[25759]	   = { EffectID = 1191 }, --Green Dragon Orb
+	[25760]	   = { EffectID = 1191 }, --Blue Dragon Orb
+	[25761]	   = { EffectID = 1191 }, --Red Dragon Orb
+	[25762]	   = { EffectID = 1191 }, --Gold Dragon Orb
+	[25763]	   = { EffectID = 1191 }, --Purple Dragon Orb
+	[25764]	   = { EffectID = 1191 }, --Silver Dragon Orb
+	[25765]	   = { EffectID = 1191 }, --Large Dragon Bone
+	--[25766]	   = { EffectID = 1191 }, --Small Dragon Bone
+	[25767]	   = { EffectID = 1191 }, --Angel's Dream
+	[25809]	   = { EffectID = 1191 }, --Redium
+	[25810]	   = { EffectID = 1191 }, --Rindium
+	[25811]	   = { EffectID = 1191 }, --Odium
+	[25812]	   = { EffectID = 1191 }, --Purdium
+	[25813]	   = { EffectID = 1191 }, --Whidium
+	[25814]	   = { EffectID = 1191 }, --Dynite
+	---Cards Missing
+	[300001]   = { EffectID = 1186 }, --Poisonous Card
+	[300002]   = { EffectID = 1186 }, --Toxious Card
+	[300003]   = { EffectID = 1186 }, --White Porcellio Card
+	[300004]   = { EffectID = 1186 }, --Neo Mineral Card
+	[300005]   = { EffectID = 1186 }, --Abyssman Card
+	[300006]   = { EffectID = 1186 }, --Jewelry Ant Card
+	[300007]   = { EffectID = 1186 }, --Jeweled Ungoliant Card
+	[300008]   = { EffectID = 1186 }, --Angelgolt Card
+	[300009]   = { EffectID = 1186 }, --Spectrum Plasma Card
+	[300010]   = { EffectID = 1186 }, --Arch Plasma Card
+	[300011]   = { EffectID = 1186 }, --Holy Frus Card
+	[300012]   = { EffectID = 1186 }, --Holy Skogul Card
+	[300013]   = { EffectID = 1186 }, --Reginleif Card
+	[300014]   = { EffectID = 1186 }, --Ingrid Card
+	[300015]   = { EffectID = 1186 }, --Purple Ferus Card
+	[300016]   = { EffectID = 1186 }, --Treasure Mimic Card
+	[300017]   = { EffectID = 1186 }, --Black Acidus Card
+	[300018]   = { EffectID = 1186 }, --Silver Acidus Card
+	[300019]   = { EffectID = 1186 }, --Bone Ferus Card
+	[300020]   = { EffectID = 1186 }, --Bone Acidus Card
+	[300021]   = { EffectID = 1186 }, --Bone Detardeurus Card
+}
