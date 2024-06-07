8aa2ec982fa98af530294cbaf50482ef58c7fca9
fishdeity <56460323+fishdeity@users.noreply.github.com>
2024-06-06 19:13:04 -0700

    changes some items' sprites

---
+++ System/customsprites.lua
@@ -0,0 +1,16 @@
+customsprites = {
+    --[[
+    ! YOU DO NOT HAVE TO RE-RUN iteminfo.bat AFTER EDITING THIS FILE !
+    ! Sprites added here will take priority over original iteminfo !
+	
+	Format:
+	[item ID] = "appearance sprite/resource name",
+	
+	You can find sprite/resource names in iteminfo.lub or ratemyserver.net
+	--]]
+	
+	-- Misc
+	[22511] = "빛의주문서", -- Fenrir's Power Scroll -> Scroll sprite
+	[4557]	= "카프라명함", -- Weakened Fenrir Card -> Kafra Card sprite
+	[6187]  = "스페어카드", -- Blank Card -> Spare Card sprite
+}	
