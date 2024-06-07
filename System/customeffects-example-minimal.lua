91e38271086af3d5a694a059313273cb039006f5
fishdeity <56460323+fishdeity@users.noreply.github.com>
2024-06-06 19:06:07 -0700

    customeffects with less stuff

---
+++ System/customeffects-example-minimal.lua
@@ -0,0 +1,33 @@
+customeffects = {
+	--[[
+	! YOU DO NOT HAVE TO RE-RUN iteminfo.bat AFTER EDITING THIS FILE !
+	! Effects added here will take priority over original iteminfo !
+	Refer to effect_list.txt for an (incomplete) list of effect IDs
+	1186: rare highlight (pink)   -has sound
+	1187: rare highlight (white)
+	1188: rare highlight (blue)
+	1189: rare highlight (gold)   -has sound
+	1190: rare highlight (purple) -has sound
+	1191: rare highlight (orange)
+	
+	1869: rare highlight (light blue) -has sound
+	1870: rare highlight (green)      -has sound
+	1871: rare highlight (red)        -has sound
+	2371: rare highlight (not working)
+	2372: rare highlight (not working)
+	105: no effect
+	--]]
+
+	--- Cards
+	[4557] = {EffectID = 1191}, -- Weakened Fenrir Card
+	
+	[22511] = {EffectID = 105}, -- Fenrir's Power Scroll
+	[6187] = {EffectID = 105}, -- Blank Card
+
+	--- Drops
+	[1000322] = {EffectID = 1190}, -- Ether Dust
+	[1000323] = {EffectID = 1190}, -- Ether Stone
+	[25503] = {EffectID = 1190}, -- Flower Left by Phantom
+	[23806] = {EffectID = 1190}, -- Ancient Heros Weapon Box
+	[12103] = {EffectID = 1190}, -- Bloody Branch
+}
