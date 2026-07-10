9ff212f7f3def1faf2472565dee7a8a4c59c2054
fishdeity <56460323+fishdeity@users.noreply.github.com>
2026-07-09 19:25:15 -0700

    S3 light beams, mvp cards have own sprite up to Chapter 1

--- System/customsprites.lua
+++ System/customsprites.lua
@@ -8,9 +8,9 @@ customsprites = {
 	
 	You can find sprite/resource names in iteminfo.lub or ratemyserver.net
 	--]]
-	
+
 	-- Misc
 	[22511] = "빛의주문서", -- Fenrir's Power Scroll -> Scroll sprite
 	[4557]	= "카프라명함", -- Weakened Fenrir Card -> Kafra Card sprite
 	[6187]  = "스페어카드", -- Blank Card -> Spare Card sprite
-}	
+}
